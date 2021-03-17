5cabbdae440ddf4ef160187ae42998c1a8100052
Gabriel Ikpaetuk <gabrielikpaetuk@byui.edu>
2021-03-17 10:56:44 -0600

    added check11b

--- check11b/pair.h
+++ check11b/pair.h
@@ -8,8 +8,28 @@
 #ifndef PAIR_H
 #define PAIR_H
 
+#include <iostream>
+using namespace std;
+
 // TODO: Fill in this class
 // Put all of your method bodies right in this file
 
+template <typename T1, typename T2>
+class Pair
+{
+private:
+    T1 first;
+    T2 second;
 
+public:
+    //Getters
+    T1 getFirst() { return first; }
+    T2 getSecond() { return second; }
+
+    //Setters
+    void setFirst(T1 value) { this->first = value; }
+    void setSecond(T2 value) { this->second = value; }
+
+    void display() { cout << first << " - " << second; }
+};
 #endif // PAIR_H
